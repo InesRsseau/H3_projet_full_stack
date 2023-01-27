c4227e4e2d2e311a601768cb0b0779b8bb435da7
InesRsseau <97746347+InesRsseau@users.noreply.github.com>
2023-01-27 15:06:53 +0100

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,47 @@
+var config = {
+  type: Phaser.AUTO,
+  parent: "phaser-example",
+  backgroundColor: "#0072bc",
+  physics: {
+    default: "arcade",
+    arcade: {
+      debug: true,
+    },
+  },
+  scene: {
+    create: create,
+    update: update,
+  },
+};
+
+var cursors;
+var player;
+
+var game = new Phaser.Game(config);
+
+function create() {
+  player = this.add.rectangle(400, 300, 64, 64, 0xffffff);
+  player2 = this.add.rectangle(100, 200, 64, 64, 0x000000);
+
+  this.physics.add.existing(player, false);
+
+  cursors = this.input.keyboard.createCursorKeys();
+
+  player.body.setCollideWorldBounds(true);
+}
+
+function update() {
+  player.body.setVelocity(0);
+
+  if (cursors.left.isDown) {
+    player.body.setVelocityX(-300);
+  } else if (cursors.right.isDown) {
+    player.body.setVelocityX(300);
+  }
+
+  if (cursors.up.isDown) {
+    player.body.setVelocityY(-300);
+  } else if (cursors.down.isDown) {
+    player.body.setVelocityY(300);
+  }
+}
---
+++ js/poo.js
@@ -0,0 +1,38 @@
+class character{
+    constructor(pv, damage_shoot, damage_chase, speed, image){
+        this.pv = pv;
+        this.damage_shoot = damage_shoot;
+        this.damage_chase = damage_chase;
+        this.speed = speed;
+        this.image = image;
+    }
+}
+
+const player = new character(10,4,0,4,'/images/characters/student/persoface.png');
+const shooter = new character(1,2,0,2,); //RAJOUTER IMAGE
+const chaser = new character (3,0,1,4); //RAJOUTER IMAGE
+const boubou = new character(6,2,2,1); //RAJOUTER IMAGE
+
+class items{
+    constructor(pv, damage, speed, image){
+        this.pv = pv;
+        this.damage = damage;
+        this.speed = speed;
+        this.image = image;
+    }
+}
+
+const i_pc = new character(0,1,-1,'/images/items/pc.png');
+const i_pingpong = new character(-1,1,0,'/images/items/pingpongraquette.png');
+const i_scarf = new character(-1,1,0,'/images/items/scarf.png');
+const i_diplome = new character(0,1,-1,'/images/items/diplome.png');
+
+const i_hotdog = new character(1,0,-1,'/images/items/hotdog.png');
+const i_bag = new character(1,-1,0,'/images/items/bag.png'); 
+const i_costume = new character(1,0,-1,'/images/items/costume.png');
+//const i_chocolate = new character(1,0,-1,'/images/items/.png'); RAJOUTER IMAGE
+
+const i_switch = new character(0,-1,1,'/images/items/switch.png');
+const i_skate = new character(-1,0,1,'/images/items/skate.png');
+const i_coke = new character(-1,0,1,'/images/items/coke.png');
+const i_chargeur = new character(0,-1,1,'/images/items/chargeur-degueu.png');
